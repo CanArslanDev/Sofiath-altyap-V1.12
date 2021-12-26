700b0ff08f2931d70c322cff32b290be836d7520
Glitch (regular-general-pufferfish) <none>
2021-12-26 13:27:11 +0000

    📍🏁 Checkpoint ./commands/cf.js:5606966/928

--- commands/cf.js
+++ commands/cf.js
@@ -6,9 +6,12 @@ exports.execute = async (client, message, args) => {
         "Street"
     ];
     let amount = Math.floor(Math.random() * 200)+50;
+    let amount2 = Math.floor(Math.random() * 200)/100;
+    let amount3 = args[1]
     let beg = await client.eco.beg(client.ecoAddUser, amount, { canLose: false, cooldown: 1, customName: "search" });
+    let beg2 = await client.eco.beg(client.ecoAddUser, amount2, { canLose: false, cooldown: 1, customName: "search" });
     if (beg.onCooldown) return message.reply(`Come back after ${beg.time.minutes} minutes & ${beg.time.seconds} seconds.`);
-    return message.reply(`**${[Math.floor(Math.random() * users.length)]}** Paranı **2'ye** katladın ve **${beg.amount}** 💸 Kazandın!. \n Şuanki Paran:**${beg.after}** 💸.`);
+    return message.reply(`**${[Math.floor(beg2.amount)]}** Paranı **2'ye** **${amount}** katladın ve **${beg.amount}** 💸 Kazandın!. \n Şuanki Paran:**${beg.after}** 💸.`);
 };
 
 exports.help = {
