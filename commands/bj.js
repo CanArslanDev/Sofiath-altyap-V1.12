084375520ef54aed56e753cc5240902cf47ba837
Glitch (regular-general-pufferfish) <none>
2021-12-26 17:49:41 +0000

    🚤🎯 Checkpoint ./commands/bj.js:5606966/5812

--- commands/bj.js
+++ commands/bj.js
@@ -10,6 +10,7 @@ exports.execute = async (client, message, args) => {
     let amount2 = Math.floor(Math.random() * 200)/100;
     let amount3 = args[0]
     let azcok=""
+    let amountfinish= 0
      if (!amount3 || isNaN(amount3)) return message.channel.send(`** ⛔${message.author.tag} | ** Lütfen Sayı Giriniz.`);
   else{
     if(amount3>21 || amount3<1) return message.channel.send(`** ⛔${message.author.tag} | ** 21'den büyük ve 1'den küçük sayı giremessiniz.`);
@@ -29,10 +30,30 @@ exports.execute = async (client, message, args) => {
         }, 1000);
     setTimeout(() => {
       let amount4 = Math.floor(Math.random() * 10)+2;
-         embed.setDescription(`**🃏${message.author.tag}= *${amount3}*💶  \n 🃏Rakip= ${amount}💶 **`)
+      let amountfinish= Math.floor(amount+amount4)
+         embed.setDescription(`**🃏${message.author.tag}= *${amount3}*💶  \n 🃏Rakip= ${amountfinish}💶 **`)
         return msg.edit(embed)
         }, 2000);
           });
+          if(amountfinish=amount3) {
+            embed.setFooter(`Rakip Kazandı :(`)
+            return embed.edit(embed)
+          }
+           if(amountfinish>amount3)
+        {
+          embed.Color("RED")
+          let data2= client.eco.removeMoney(client.ecoAddUser, parseInt(amount3));
+          embed.setFooter(`Rakip Kazandı :(`)
+          return embed.edit(embed)
+        }
+      else
+        {
+          embed.Color("GREEN")
+          let data2= client.eco.addMoney(client.ecoAddUser, parseInt(amount3));
+          embed.setFooter(`Tebrikler ,Kazandın!`)
+          return embed.edit(embed)
+        }
+    
   }
 };
 
