e46331e088729cf08c13d380e6021f2cef9e794e
Glitch (regular-general-pufferfish) <none>
2021-12-26 14:02:13 +0000

    🏮🙎 Checkpoint ./commands/cf1.js:5606966/716

--- commands/cf1.js
+++ commands/cf1.js
@@ -4,11 +4,18 @@ exports.execute = async (client, message, args) => {
     let amount3 = args[0]
     let authordata = client.eco.fetchMoney(message.author.id) 
     let beg = await client.eco.beg(client.ecoAddUser, amount, { canLose: false, cooldown: 1, customName: "search" });
-    let beg2 = await client.eco.beg(client.ecoAddUser, amount2, { canLose: false, cooldown: 1, customName: "search" });
     if (beg.onCooldown) return message.reply(`Come back after ${beg.time.minutes} minutes & ${beg.time.seconds} seconds.`);
-    if(beg.before>amount3) return messsage.reply(`Girdiğiniz miktar paranızdan fazla olamaz`)
-    else return message.reply(`Paranı **2'ye** katladın ve **${amount}**💸 Kazandın!. \n Şuanki Paran:**${beg.after}** 💸.\n kullanıcıdan alınan veri:**${amount3}** \n Şans: **${[Math.floor(beg2.amount)]}** \n Kullanıcı: **${beg.before}**`);
-};
+    if(beg.before<amount3) 
+    {
+      return message.reply(`Girdiğiniz miktar paranızdan fazla olamaz`)
+    }
+    else
+    {
+      let beg = await client.eco.beg(client.ecoAddUser, amount, { canLose: false, cooldown: 1, customName: "search" });
+      return message.reply(`Paranı **2'ye** katladın ve **${amount}**💸 Kazandın!. \n Şuanki Paran:**${beg.after}** 💸.\n kullanıcıdan alınan veri:**${amount3}** \n Şans: **${[Math.floor(amount2)]}** \n Kullanıcı: **${authordata.amount}**`);
+    }
+    
+    };
 
 exports.help = {
     name: "cf1",
