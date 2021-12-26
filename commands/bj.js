ff16c247f35870162b94f91280323ecacf9b9d3d
Glitch (regular-general-pufferfish) <none>
2021-12-26 16:37:05 +0000

    🏪😚 Checkpoint ./commands/bj.js:5606966/320 ./commands/cf.js:5606966/179

--- commands/bj.js
+++ commands/bj.js
@@ -6,14 +6,15 @@ exports.execute = async (client, message, args) => {
         "Zero's Databse",
         "Street"
     ];
-    let amount = Math.floor(Math.random() * 200)+50;
+    let amount = Math.floor(Math.random() * 15);
     let amount2 = Math.floor(Math.random() * 200)/100;
     let amount3 = args[0]
-     if (!amount3 || isNaN(amount3)) return message.channel.send(`** ⛔${message.author.tag} | ** Lütfen Sayı Giriniz`);
+     if (!amount3 || isNaN(amount3)) return message.channel.send(`** ⛔${message.author.tag} | ** Lütfen Sayı Giriniz.`);
   else{
+    if(amount3>21) return message.channel.send(`** ⛔${message.author.tag} | ** 21'den büyük sayı giremessiniz.`);
    const embed = new MessageEmbed()
   .setTitle(`**${message.author.tag} | ${amount3} İle Blackjack Oynadı!**`)
-  .setDescription(`**${message.author.tag}= ${amount3} **`)
+  .setDescription(`**${message.author.tag}= ${amount3}💶 ** \n **Rakip= ${amount}💶 **`)
   .setColor("GRAY")
   .setFooter("Do :?buy <item> to purchase the item.")
   return message.channel.send(embed);
--- commands/cf.js
+++ commands/cf.js
@@ -8,14 +8,14 @@ exports.execute = async (client, message, args) => {
     let amount = Math.floor(Math.random() * 200)+50;
     let amount2 = Math.floor(Math.random() * 200)/100;
     let amount3 = args[0]
-    let beg = await client.eco.beg(client.ecoAddUser, amount, { canLose: false, cooldown: 1, customName: "search" });
-    let beg2 = await client.eco.beg(client.ecoAddUser, amount2, { canLose: false, cooldown: 1, customName: "search" });
-    if (beg.onCooldown) return message.reply(`Come back after ${beg.time.minutes} minutes & ${beg.time.seconds} seconds.`);
-    return message.reply(`**${message.author.tag} | ** **${[Math.floor(beg2.amount)]}** Paranı **2'ye** **${amount}** katladın ve **${amount3}** 💸 Kazandın!. \n Şuanki Paran:**${beg.after}** 💸.`);
+   // let beg = await client.eco.beg(client.ecoAddUser, amount, { canLose: false, cooldown: 1, customName: "search" });
+   // let beg2 = await client.eco.beg(client.ecoAddUser, amount2, { canLose: false, cooldown: 1, customName: "search" });
+  //  if (beg.onCooldown) return message.reply(`Come back after ${beg.time.minutes} minutes & ${beg.time.seconds} seconds.`);
+  //  return message.reply(`**${message.author.tag} | ** **${[Math.floor(beg2.amount)]}** Paranı **2'ye** **${amount}** katladın ve **${amount3}** 💸 Kazandın!. \n Şuanki Paran:**${beg.after}** 💸.`);
 };
 
 exports.help = {
-    name: "cf",
+    name: "test",
     aliases: [],
-    usage: "cf <amount>"
+    usage: "test2 <amount>"
 }
