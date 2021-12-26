83e1cf7787424efeef85be907912161ad1851958
Glitch (regular-general-pufferfish) <none>
2021-12-26 14:37:31 +0000

    👹🌋 Checkpoint ./commands/cf1.js:5606966/585

--- commands/cf1.js
+++ commands/cf1.js
@@ -2,19 +2,18 @@ exports.execute = async (client, message, args) => {
     let amount = Math.floor(Math.random() * 200)+50;
     let amount2 = Math.floor(Math.random() * 200)/100;
     let amount3 = args[0]
+    let yazitura= ""
     let authordata = client.eco.fetchMoney(message.author.id) 
-    if(authordata.amount<amount3) 
-    {
-      return message.reply(`Girdiğiniz miktar paranızdan fazla olamaz`)
-    }
+    if(authordata.amount<amount3)  return message.reply(`Girdiğiniz miktar paranızdan fazla olamaz`);
     else
     {
       if([Math.floor(amount2)]==0)
         {
-          
-            let beg = await client.eco.beg(client.ecoAddUser, amount3, { canLose: false, cooldown: 1, customName: "search" });
+          if([Math.floor(amount2)]==0) yazitura="Tura";
+          else yazitura="Yazı";
+            let beg = await client.eco.beg(client.ecoAddUser, amount3*2, { canLose: false, cooldown: 1, customName: "search" });
       if (beg.onCooldown) return message.reply(`Come back after ${beg.time.minutes} minutes & ${beg.time.seconds} seconds.`);
-      return message.channel.send(`**${message.author.tag}**Paranı **2'ye** katladın ve **${amount}**💸 Kazandın!. \n Şuanki Paran:**${beg.after}** 💸.\n kullanıcıdan alınan veri:**${amount3}** \n Şans: **${[Math.floor(amount2)]}** \n Kullanıcı ismi: **${message.author.tag}**`);
+      return message.channel.send(`**${message.author.tag} | ** **${amount3}**💶 oynadı ve *${yazitura}** seçti! \n Para döndürülüyor...🪙 ve **${(amount3)*2}** Kazandın! \n Paranı **2'ye** katladın ve **${amount}**💸 Kazandın!. \n Şuanki Paran:**${beg.after}** 💸.\n kullanıcıdan alınan veri:**${amount3}** \n Şans: **${[Math.floor(amount2)]}** \n Kullanıcı ismi: **${message.author.tag}**`);
         }
     else return message.channel.send(`**${message.author.tag}** kaybettiniz :c`)
     }
